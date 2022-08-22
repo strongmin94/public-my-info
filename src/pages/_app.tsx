6f148e40bd7c5bc863f19e31a1b832dc6bf690d5
Matt <strongmin94@gmail.com>
2022-08-22 16:31:08 +0900

    Next app initialize

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from "next/app";
+import GlobalStyles from "../styles/globalStyles";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
